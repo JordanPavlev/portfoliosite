2976748a1c8ac0a4ef98e5b939bdf256a141f198
JordanPavlev <danipavlev@gmail.com>
2022-06-17 17:14:25 +0300

    no internet again

---
+++ src/components/Layout/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Sidebar from '../Sidebar';
+import './index.scss';
+
+const Layout = () => {
+    return <><Sidebar/></>
+}
+
+export default Layout;
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,9 @@
+import './index.scss'
+
+const Sidebar = () => {
+    return (
+        <h1>Hello from sidebar</h1>
+    )
+}
+
+export default Sidebar;
